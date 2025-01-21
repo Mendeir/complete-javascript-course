d3fefa3e2757a7bb67794f771e5f0a6f1a61549b
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-21 12:47:47 +0800

    Upload Fundamentals Part 1 sandbox file

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+let firstName = "MG";
+console.log(firstName);
+
+const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+const tip = bill >= 50 && bill <= 300 ? .15 : .20;
+
+console.log(`The bill was ${bill}, the tip was ${bill * tip}, and the total value ${Number(bill + bill * tip)}`);

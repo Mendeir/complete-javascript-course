476a1fac3602e45dc4f350a923a30856773fbcc8
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-22 19:58:08 +0800

    Guess my number - change the color of background to green and widen the number if user won the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,6 +19,8 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
     }
 
     if (guess > secretNumber) {

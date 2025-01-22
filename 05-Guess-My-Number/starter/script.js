f2b927578ebc35482b2e586826f3ad2384fe7073
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-22 20:51:31 +0800

    Guess my number - implement again button functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +19,7 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
     }
 
     if (guess > secretNumber) {
@@ -34,4 +33,16 @@ document.querySelector('.check').addEventListener('click', function () {
         score--;
         document.querySelector('.score').textContent = score;
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
 });

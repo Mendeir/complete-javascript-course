c2c065b84dcea114dabc95676237b982c80886f6
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-22 19:52:01 +0800

    Guess my number - implement initial game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,35 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
+    if (score <= 0) {
+        document.querySelector('.message').textContent = 'You lost the game!';
+        return;
+    }
+
     if (!guess) {
         document.querySelector('.message').textContent = 'No number!';
+    }
+
+    if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    }
+
+    if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+
+    if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
     }
 });

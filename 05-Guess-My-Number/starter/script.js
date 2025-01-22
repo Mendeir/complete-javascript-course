18fd17961a8aafaa52d460df3a42a0a28b491eef
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-22 20:56:36 +0800

    Guess my number - add highscore feature

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -16,10 +17,15 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     if (guess === secretNumber) {
+        if (score > highScore) {
+            highScore = score;
+        }
+
         document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.highscore').textContent = highScore;
     }
 
     if (guess > secretNumber) {
@@ -38,7 +44,6 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     score = 20;
-
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';

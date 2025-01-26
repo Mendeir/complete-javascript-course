2602a0313b22dbb514f7350b67d7501962a49489
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-26 20:06:47 +0800

    Data structures - finish assignment for array destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -252,3 +252,24 @@ const books = [
         highlighted: true,
     },
 ];
+
+//1.1
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+
+//1.2
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+//1.3
+const ratings = [
+    ['rating', 4.19],
+    ['ratingsCount', 144584],
+];
+const [[, rating], [, ratingsCount]] = ratings;
+console.log(rating, ratingsCount);
+
+//1.4
+const ratingStars = [63405, 1808];
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);

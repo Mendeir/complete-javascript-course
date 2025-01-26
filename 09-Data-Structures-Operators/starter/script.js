7f0755bb081c5cc084d63f57f8720e4fd0ace028
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-26 21:23:59 +0800

    Data strcutures - finish assignment for rest pattern and parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -325,3 +325,19 @@ const spellWord = function (givenString) {
     console.log(...givenString);
 };
 spellWord('JavaScript');
+
+console.log('SECTION 4');
+
+//4.1
+const [mainKeyword, ...rest] = books[0].keywords;
+console.log(mainKeyword, rest);
+
+//4.2
+const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+console.log(bookPublisher, restOfTheBook);
+
+//4.3
+const printBookAuthorsCount = function (title, ...authors) {
+    console.log(`"The book "${title}" has ${authors.length} authors`);
+};
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');

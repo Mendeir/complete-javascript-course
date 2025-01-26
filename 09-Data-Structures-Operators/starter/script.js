8a3c1f64c111c23ede338f926d12052d26beb689
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-26 21:37:15 +0800

    Data structures - finish assignment for nullish coalescing operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -354,3 +354,13 @@ for (let i = 0; i < books.length; i++) {
     books[i].onlineContent &&
         console.log(`"${books[i].title}" provides online content`);
 }
+
+console.log('SECTION 6');
+
+//6.1
+for (let i = 0; i < books.length; i++) {
+    books[i].onlineContent ??
+        console.log(
+            `"${books[i].title} provides no data about its online content"`,
+        );
+}

00e55ed964776816cf50f1f7e8a43c9f6a0b5539
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-27 19:38:54 +0800

    Data structures - finish assignment for enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -484,3 +484,28 @@ console.log(allAuthors);
 for (const [index, author] of allAuthors.entries()) {
     console.log(`${index + 1}: ${author}`);
 }
+
+console.log('SECTION 9');
+//9.1
+const bookData = [
+    ['title', 'Computer Networking: A Top-Down Approach'],
+    ['author', ['James F. Kurose', 'Keith W. Ross']],
+    ['publisher', 'Addison Wesley'],
+];
+
+const newBook = {
+    [bookData[0][0]]: bookData[0][1],
+    [bookData[1][0]]: bookData[1][1],
+    [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+
+//9.2
+const pages = 880;
+
+const newBook2 = {
+    title: 'The C Programming Language',
+    author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+    pages,
+};
+console.log(newBook2);

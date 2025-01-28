ac73dde55c81ec14917fc965c719a0be88c34647
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-01-28 20:47:41 +0800

    Data structures - finish assignment for maps fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -625,3 +625,24 @@ console.log(uniqueKeywordsArr);
 //12.6
 uniqueKeywords.clear();
 console.log(uniqueKeywords);
+
+console.log('SECTION 13');
+//13.1
+const bookMap = new Map([
+    ['title', 'Clean Code'],
+    ['author', 'Robert C. Martin'],
+]);
+console.log(bookMap);
+
+//13.2
+bookMap.set('pages', 464);
+console.log(bookMap);
+
+//13.3
+console.log(`"${bookMap.get('title')}" by ${bookMap.get('author')}`);
+
+//13.4
+console.log(bookMap.size);
+
+//13.5
+if (bookMap.has('author')) console.log('The author is known');

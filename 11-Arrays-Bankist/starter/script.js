67d2bbfd2512e4f079e8d4aed9b9f19a36d69b93
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-02-05 16:49:45 +0800

    Bankist - add functionality to calculate current balance for a single account

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -80,6 +80,13 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+    const balance = movements.reduce((sum, movevement) => sum + movevement, 0);
+    labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accounts) {
     accounts.forEach(function (account) {
         account.username = account.owner
@@ -91,7 +98,6 @@ const createUsernames = function (accounts) {
 };
 
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////

6c82be379902f0fc7d687406f62ffdc0cbd287e1
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-02-05 20:06:16 +0800

    Bankist - finish challenge 3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -220,4 +220,21 @@ const calcAverageHumanAge = function (dogAges) {
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
+console.log('CHALLENGE 3');
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+// as an arrow function, and using chaining!
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+// GOOD LUCK 😀
+
+const calcAverageHumanAgeEnhanced = dogAges =>
+    dogAges
+        .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+        .filter(age => age >= 18)
+        .reduce((sum, age, _, arr) => sum + age / arr.length, 0);
+
+console.log(calcAverageHumanAgeEnhanced([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAgeEnhanced([16, 6, 10, 5, 6, 1, 4]));
+
 /////////////////////////////////////////////////

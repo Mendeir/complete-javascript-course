010cabd643ff5f895a1536baa5e1e34bd1f3b41f
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-03-11 13:16:15 +0800

    Bankist Advanced DOM - improve sticky scroll event

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -82,12 +82,21 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
-const initialCoords = section1.getBoundingClientRect();
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
-window.addEventListener('scroll', function (e) {
-    if (window.scrollY > initialCoords.top) {
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
         nav.classList.add('sticky');
     } else {
         nav.classList.remove('sticky');
     }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);

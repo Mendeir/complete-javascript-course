11720dff6d7b2bca39537c99b7836cff5888e3b1
Adrianne Magracia <adriannemagracia.git@gmail.com>
2025-03-13 20:52:08 +0800

    OOP - finish challenge 4

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -152,3 +152,73 @@ GOOD LUCK 😀
     tesla.brake();
     tesla.accelerate();
 }
+
+{
+    ///////////////////////////////////////
+    // Coding Challenge #4
+    /* 
+    1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+    2. Make the 'charge' property private;
+    3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+    DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+    GOOD LUCK 😀
+    */
+
+    class Car {
+        constructor(make, speed) {
+            this.make = make;
+            this.speed = speed;
+        }
+
+        get speedUS() {
+            this.speed / 1.6;
+        }
+
+        set speedUS(speed) {
+            this.speed = speed * 1.6;
+        }
+
+        accelerate() {
+            this.speed += 10;
+            console.log(this.speed);
+        }
+
+        brake() {
+            this.speed -= 5;
+            console.log(this.speed);
+        }
+    }
+
+    class EV extends Car {
+        #charge;
+
+        constructor(make, speed, charge) {
+            super(make, speed);
+            this.#charge = charge;
+        }
+
+        chargeBattery(chargeTo) {
+            this.#charge = chargeTo;
+            return this;
+        }
+
+        accelerate() {
+            this.speed += 20;
+            this.#charge -= 1;
+            console.log(
+                `${this.make} is going at ${this.speed} km/h, with a charge of ${this.#charge}%`,
+            );
+            return this;
+        }
+
+        brake() {
+            this.speed -= 20;
+            return this;
+        }
+    }
+
+    const rivian = new EV('Rivian', 120, 23);
+    rivian.chargeBattery(50).accelerate().brake().accelerate();
+}
